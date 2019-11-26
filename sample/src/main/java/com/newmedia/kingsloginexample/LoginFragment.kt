5e4b9921f970a86b50fe1bb6663a60667c217033
marcin.wiktorowski <marcin.wiktorowski@appunite.com>
2019-11-26 10:06:32 +0100

    Updated to AndroidX

--- sample/src/main/java/com/newmedia/kingsloginexample/LoginFragment.kt
+++ sample/src/main/java/com/newmedia/kingsloginexample/LoginFragment.kt
@@ -2,11 +2,11 @@ package com.newmedia.kingsloginexample
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.newmedia.kingslogin.CallbackManager
 import com.newmedia.kingslogin.KingsLoginCallback
 import com.newmedia.kingslogin.KingsLoginException
